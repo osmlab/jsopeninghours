9183537fbb9240c712542916b16ded52f65aa856
Ian Dees <ian.dees@gmail.com>
2013-05-02 12:55:30 -0500

    This seems to work for the most common usecases.

--- index.js
+++ index.js
@@ -1,7 +1,48 @@
-var days = ['su', 'mo', 'tu', 'we', 'th', 'fr', 'sa'];
+var days = ['mo', 'tu', 'we', 'th', 'fr', 'sa', 'su'];
 
 module.exports = function(text) {
     if (text == '24/7') {
-        return ['24/7'];
+        return {'24/7': true};
+    }
+    else if (text == 'seasonal') {
+        return {'seasonal': true};
+    }
+    else {
+        var result = {};
+        for (var k = 0; k < days.length; k++) {
+            result[days[k]] = null;
+        }
+
+        var dayranges = text.toLowerCase().split(';');
+        for (var i = 0; i < dayranges.length; i++) {
+            var dayrange = dayranges[i],
+                daytimes = dayrange.split(' ');
+
+            var left = daytimes[0].split('-'),
+                right;
+            if (daytimes.length === 2) {
+                right = daytimes[1].split('-');
+            }
+
+            var startday = 0,
+                endday = 6;
+
+            if (days.indexOf(left[0]) >= 0) {
+                startday = days.indexOf(left[0]);
+            }
+            if (left.length === 1) {
+                endday = startday;
+            }
+            if (left.length === 2 && days.indexOf(left[1]) >= 0) {
+                endday = days.indexOf(left[1]);
+            }
+
+            if (right.length === 2) {
+                for (var j = startday; j <= endday; j++) {
+                    result[days[j]] = [right[0], right[1]];
+                }
+            }
+            console.log(result);
+        }
     }
 };
