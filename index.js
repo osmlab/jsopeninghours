9bb84e0e39f8aff09ffa80c77381680ccfb7fec3
Ian Dees <ian.dees@gmail.com>
2013-05-03 21:51:46 -0500

    Make a page for testing various values.

--- index.js
+++ index.js
@@ -1,74 +1,76 @@
 var days = ['mo', 'tu', 'we', 'th', 'fr', 'sa', 'su'];
 
-module.exports = function(text) {
-    var result = null;
-    if (text === '' || text === null) {
-        result = null;
-    }
-    else if (text == '24/7') {
-        result = {'24/7': true};
-    }
-    else if (text == 'seasonal') {
-        result = {'seasonal': true};
-    }
-    else {
-        result = {};
-        var modified_some_days = false;
-        for (var k = 0; k < days.length; k++) {
-            result[days[k]] = null;
+(function(exports){
+    exports.compute = function(text) {
+        var result = null;
+        if (text === '' || text === null) {
+            result = null;
         }
+        else if (text == '24/7') {
+            result = {'24/7': true};
+        }
+        else if (text == 'seasonal') {
+            result = {'seasonal': true};
+        }
+        else {
+            result = {};
+            var modified_some_days = false;
+            for (var k = 0; k < days.length; k++) {
+                result[days[k]] = null;
+            }
 
-        var dayregex = /^(mo|tu|we|th|fr|sa|su)\-?(mo|tu|we|th|fr|sa|su)?$/,
-            timeregex = /^\s*(\d\d:\d\d)\-(\d\d:\d\d)\s*$/,
-            dayranges = text.toLowerCase().split(/\s*;\s*/),
-            dayrange;
-        while((dayrange = dayranges.shift())) {
-            var daytimes = dayrange.trim().split(/\s+/),
-                daytime,
-                startday = 0,
-                endday = 6,
-                whichDays,
-                whichTimes,
-                starttime,
-                endtime;
+            var dayregex = /^(mo|tu|we|th|fr|sa|su)\-?(mo|tu|we|th|fr|sa|su)?$/,
+                timeregex = /^\s*(\d\d:\d\d)\-(\d\d:\d\d)\s*$/,
+                dayranges = text.toLowerCase().split(/\s*;\s*/),
+                dayrange;
+            while((dayrange = dayranges.shift())) {
+                var daytimes = dayrange.trim().split(/\s+/),
+                    daytime,
+                    startday = 0,
+                    endday = 6,
+                    whichDays,
+                    whichTimes,
+                    starttime,
+                    endtime;
 
-            while((daytime = daytimes.shift())) {
-                if (dayregex.test(daytime)) {
-                    var daymatches = daytime.match(dayregex);
+                while((daytime = daytimes.shift())) {
+                    if (dayregex.test(daytime)) {
+                        var daymatches = daytime.match(dayregex);
 
-                    if (daymatches.length === 3) {
-                        startday = days.indexOf(daymatches[1]);
-                        if (daymatches[2]) {
-                            endday = days.indexOf(daymatches[2]);
+                        if (daymatches.length === 3) {
+                            startday = days.indexOf(daymatches[1]);
+                            if (daymatches[2]) {
+                                endday = days.indexOf(daymatches[2]);
+                            } else {
+                                endday = startday;
+                            }
                         } else {
-                            endday = startday;
+                            return null;
+                        }
+                    } else if (timeregex.test(daytime)) {
+                        var timematches = daytime.match(timeregex);
+
+                        if (timematches.length === 3) {
+                            starttime = timematches[1];
+                            endtime = timematches[2];
+                        } else {
+                            return null;
                         }
                     } else {
                         return null;
                     }
-                } else if (timeregex.test(daytime)) {
-                    var timematches = daytime.match(timeregex);
+                }
 
-                    if (timematches.length === 3) {
-                        starttime = timematches[1];
-                        endtime = timematches[2];
-                    } else {
-                        return null;
-                    }
-                } else {
-                    return null;
+                for (var j = startday; j <= endday; j++) {
+                    result[days[j]] = [starttime, endtime];
+                    modified_some_days = true;
+                }
+
+                if (!modified_some_days) {
+                    result = null;
                 }
             }
-
-            for (var j = startday; j <= endday; j++) {
-                result[days[j]] = [starttime, endtime];
-                modified_some_days = true;
-            }
-
-            if (!modified_some_days) {
-                result = null;
-            }
         }
-    }
-    return result;
-};
+        return result;
+    };
+})(typeof exports === 'undefined'? this['jsopeninghours']={}: exports);
